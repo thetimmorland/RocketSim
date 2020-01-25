6c339409e7dabc06c7934bd8dcb35d3ba6130f08
Timothy Morland <thetimmorland@gmail.com>
2020-01-25 11:03:34 -0500

    create schema and update readme

--- rsim-backend/rsim-backend/__main__.py
+++ rsim-backend/rsim-backend/__main__.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from schema import Schema, And, Use
 import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-      return 'hello world'
+rocketSchema = Schema({
+      'body': {
+            'diameter': int,
+            'length': int,
+            'mass': int,
+      }, 'fins': {
+            'sweep': int,
+            'count': int,
+            'height': int,
+            'mass': int,
+            'cant': int,
+      }, 'variableMass': {
+            'distanceFromTip': int,
+            'mass': int,
+      }, 'motor' : {
+            'impulse': int,
+            'mass': int,
+      }, 'noseCone': {
+            'length': int,
+            'mass': int,
+      },
+})
+
+@app.route('/', methods = ['GET'])
+def rocketSim():
+      rocket = rocketSchema.validate(request.json)
+      return jsonify(content)
+
 
 if __name__ == '__main__':
       app.run(debug=os.getenv("FLASK_DEBUG") or True)
