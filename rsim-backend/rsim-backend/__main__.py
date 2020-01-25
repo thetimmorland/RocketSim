05d0a7493787d97c59693278cfbe84df21d51aba
Timothy Morland <thetimmorland@gmail.com>
2020-01-25 12:52:47 -0500

    add material validation

--- rsim-backend/rsim-backend/__main__.py
+++ rsim-backend/rsim-backend/__main__.py
@@ -1,20 +1,30 @@
 from flask import Flask, request, jsonify
 from schema import Schema, And, Use
+from math import log
 import os
 
 app = Flask(__name__)
+
+materials = [
+      'aluminum',
+      'wood',
+      'cardboard',
+      'abs',
+]
 
 rocketSchema = Schema({
       'body': {
             'diameter': int,
             'length': int,
             'mass': int,
+            'material': And(Use(str), lambda m: m in materials)
       }, 'fins': {
             'sweep': int,
             'count': int,
             'height': int,
             'mass': int,
             'cant': int,
+            'material': And(Use(str), lambda m: m in materials)
       }, 'variableMass': {
             'distanceFromTip': int,
             'mass': int,
@@ -24,15 +34,15 @@ rocketSchema = Schema({
       }, 'noseCone': {
             'length': int,
             'mass': int,
+            'material': And(Use(str), lambda m: m in materials)
       },
 })
-
 
 
 @app.route('/', methods = ['POST'])
 def rocket_sim():
       rocket = rocketSchema.validate(request.json)
-      return jsonify(content)
+      return jsonify(output)
 
 
 if __name__ == '__main__':
