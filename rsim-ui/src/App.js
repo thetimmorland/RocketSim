3b3b100764b7645b810f1266db69951a1073ceaf
Andrew Fryer <andrew.fryer@queensu.ca>
2020-01-26 02:35:38 +0000

    pull graph logic into another file

--- rsim-ui/src/App.js
+++ rsim-ui/src/App.js
@@ -7,9 +7,8 @@ import GridLayout from 'react-grid-layout';
 import '../node_modules/react-vis/dist/style.css';
 import '../node_modules/react-grid-layout/css/styles.css';
 import '../node_modules/react-resizable/css/styles.css';
-import {XYPlot, VerticalGridLines, HorizontalGridLines, XAxis, YAxis, LineSeries} from 'react-vis';
 import CSVReader from "react-csv-reader";
-import Derivative from './Derivative';
+import Graph from "./Graph";
 
 const inputsStructure_manual = [
   {
@@ -104,56 +103,6 @@ const inputsStructure_manual = [
 
 const inputsStructure_preFilled = [{"className":"body","header":"body","inputs":[{"name":"diameter","value":34},{"name":"length","value":46},{"name":"mass","value":59},{"name":"material","value":"cardboard"}]},{"className":"fins","header":"fins","inputs":[{"name":"cant","value":15},{"name":"count","value":7},{"name":"height","value":29},{"name":"mass","value":52},{"name":"material","value":"aluminum"},{"name":"sweep","value":14}]},{"className":"variableMass","header":"variableMass","inputs":[{"name":"distanceFromTip","value":18},{"name":"mass","value":10}]},{"className":"motor","header":"motor","inputs":[{"name":"impulse","value":53},{"name":"mass","value":72},{"name":"burnTime","value":86}]},{"className":"noseCone","header":"noseCone","inputs":[{"name":"length","value":33},{"name":"mass","value":26},{"name":"material","value":"abs"}]}];
 
-const Results = props =>  {
-  const layout = [
-    {i: 'a', x: 0, y: 0, w: 2, h: 1, static: true},
-    {i: 'b', x: 1, y: 0, w: 1, h: 1, minW: 2, maxW: 4},
-    {i: 'c', x: 4, y: 0, w: 1, h: 2}
-  ];
-
-  const d1 = props.data;
-  const d2 = props.data2;
-  const d1_ = Derivative(d1);
-  const d2_ = Derivative(d2);
-  const d1__ = Derivative(d1_);
-  const d2__ = Derivative(d2_);
-
-  return (
-    <GridLayout className="layout" layout={layout} cols={3} rowHeight={300} width={window.innerWidth} height={window.innerHeight}>
-      <div key="a" className="Results">
-        <XYPlot className="plot" height={(layout[0]["h"] * 300) - 40} width={(window.innerWidth/3) * layout[0]["w"] - 30}>
-          <VerticalGridLines />
-          <HorizontalGridLines />
-          <XAxis title="Time (s)"/>
-          <YAxis title="Altitude"/>
-          <LineSeries data={d1} />
-          <LineSeries data={d2} />
-        </XYPlot>
-      </div>
-      <div key="b" className="Results">
-        <XYPlot className="plot" height={(layout[0]["h"] * 300) - 40} width={(window.innerWidth/3) * layout[0]["w"] - 30}>
-          <VerticalGridLines />
-          <HorizontalGridLines />
-          <XAxis title="Time (s)"/>
-          <YAxis title="Velocity"/>
-          <LineSeries data={d1_} />
-          <LineSeries data={d2_} />
-        </XYPlot>
-      </div>
-      <div key="c" className="Results">
-        <XYPlot className="plot" height={(layout[0]["h"] * 300) - 40} width={(window.innerWidth/3) * layout[0]["w"] - 30}>
-          <VerticalGridLines />
-          <HorizontalGridLines />
-          <XAxis title="Time (s)"/>
-          <YAxis title="Acceleration"/>
-          <LineSeries data={d1__} />
-          <LineSeries data={d2__} />
-        </XYPlot>
-      </div>
-    </GridLayout>
-  )
-};
-
 const reformatRes = res => res.map(dataPoint => ({
   x: dataPoint[0],
   y: dataPoint[1][0],
@@ -232,9 +181,9 @@ export default function App() {
       <Button onClick={() => simulate(state, setResults)}>Simulate!</Button>
       <CSVReader onFileLoaded={data => setResults2(data.map(row => ({
         x: row[0],
-        y: row[1], 
+        y: row[1],
       })))}/>
-      <Results data={results} data2={results2}/>
+      <Graph data={results} data2={results2}/>
     </div>
   );
 };
