313e26bc1ddd29e44941fbfbdd4c896c9d4552c1
Timothy Morland <thetimmorland@gmail.com>
2020-01-25 21:51:48 -0500

    rewrite of flight model, now produces altitude not velocity

--- rsim-backend/rsim-backend/__main__.py
+++ rsim-backend/rsim-backend/__main__.py
@@ -1,23 +1,28 @@
 from flask import Flask, request, jsonify
 from schema import Schema, And, Use
-from scipy.integrate import odeint
+from scipy.integrate import odeint, cumtrapz
 import math
 import numpy as np
 import os
 
 app = Flask(__name__)
 
-dragCoefficients = {
+DRAG_COEFFICIENTS = {
       'abs': 0.25,
       'aluminum': 1.4,
       'cardboard': 0.072,
       'wood': 0.4,
 }
 
+STD_GRAVITY = 9.80665
+
+AIR_DENSITY = 1.225
+
+
 positiveInt = And(int, lambda n: n > 0)
 positiveFloat = And(Use(float), lambda n: n > 0)
 validMaterial = And(Use(str), lambda s:
-                    s in dragCoefficients.keys())
+                    s in DRAG_COEFFICIENTS.keys())
 
 rocketSchema = Schema({
       'body': {
@@ -42,16 +47,25 @@ rocketSchema = Schema({
       },
 }, ignore_extra_keys=True)
 
-def bodyDragCoefficient(body):
-      return body['diameter'] * body['length'] * dragCoefficients[body['material']]
 
-def finsDragCoefficient(fins):
-      return 2 * fins['count'] * fins['height']**2 * math.sin(fins['sweep']) * \
-            dragCoefficients[fins['material']]
+@app.route('/', methods = ['POST'])
+def rocket_sim():
+      rocket = rocketSchema.validate(request.json)
 
-def noseConeDragCoefficient(radius, height, material):
-      return math.pi * radius * math.sqrt(height**2 + radius**2) * dragCoefficients[material]
+      def model(t, v):
+            return exaustVelocity(rocket) \
+                  + math.log(initalNetMass(rocket) / netMass(rocket, t)) \
+                  - STD_GRAVITY - (AIR_DENSITY * v**2 * dragCoefficient(rocket))
 
+      t = np.linspace(0, 100)
+      v = odeint(model, 0, t).flatten()
+
+      altitude = cumtrapz(v, x=t)
+      return jsonify(list(zip(t.tolist(), altitude.tolist())))
+
+
+def exaustVelocity(rocket):
+      return STD_GRAVITY * rocket['motor']['impulse']
 
 def initalNetMass(rocket):
       return sum([rocket['body']['mass'],
@@ -60,47 +74,31 @@ def initalNetMass(rocket):
                   rocket['noseCone']['mass']])
 
 def netMass(rocket, time):
-      return initalNetMass(rocket) - rocket['motor']['mass'] * time / rocket['motor']['burnTime']
+      if (time < rocket['motor']['burnTime']):
+            return initalNetMass(rocket) \
+                  - time / rocket['motor']['burnTime'] * rocket['motor']['mass']
+      else:
+            return initalNetMass(rocket) - rocket['motor']['mass']
 
-def accelThrust(rocket, time):
-      standardGravity = 9.81
+def dragCoefficient(rocket):
+      bodyDragCoefficient = rocket['body']['diameter'] * rocket['body']['length'] \
+            * DRAG_COEFFICIENTS[rocket['body']['material']]
 
-      return rocket['motor']['impulse'] * standardGravity * \
-            math.log(initalNetMass(rocket) / netMass(rocket, time))
+      finDragCoefficient = 2 * rocket['fins']['count'] * rocket['fins']['height']**2 \
+            * math.sin(rocket['fins']['sweep']) * DRAG_COEFFICIENTS[rocket['fins']['material']]
 
-def accelDrag(rocket, velocity):
-      airDensity = 1.225
+      noseConeRadius = rocket['body']['diameter'] / 2
+      noseDragCoefficient = math.pi * noseConeRadius \
+            * math.sqrt(rocket['noseCone']['length']**2 + noseConeRadius**2) \
+            * DRAG_COEFFICIENTS[rocket['noseCone']['material']]
 
-      netDragCoefficient = sum([
-            bodyDragCoefficient(rocket['body']),
-            finsDragCoefficient(rocket['fins']),
-            noseConeDragCoefficient(
-                  rocket['body']['diameter'] / 2,
-                  rocket['noseCone']['length'],
-                  rocket['noseCone']['material'],
-            )
+      return sum([
+            bodyDragCoefficient,
+            finDragCoefficient,
+            noseDragCoefficient,
       ])
 
       return airDensity * velocity**2 * netDragCoefficient / 2
-
-@app.route('/', methods = ['POST'])
-def rocket_sim():
-      rocket = rocketSchema.validate(request.json)
-
-      def burnModel(v, t):
-
-            dvdt = accelThrust(rocket, t) - accelDrag(rocket, v)
-
-            return dvdt
-
-      t = np.linspace(0, rocket['motor']['burnTime'])
-      v = odeint(burnModel, 0, t)
-
-      t = t.tolist()
-      v = v.tolist()
-
-      return jsonify(list(zip(t, v)))
-
 
 if __name__ == '__main__':
       app.run(debug=os.getenv("FLASK_DEBUG") or True)
