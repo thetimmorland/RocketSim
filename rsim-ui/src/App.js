9397ba55d05b4799cca9d34a8e2be80a3c07b883
Andrew Fryer <andrew.fryer@queensu.ca>
2020-01-26 02:27:11 +0000

    graph the derivatives

--- rsim-ui/src/App.js
+++ rsim-ui/src/App.js
@@ -9,6 +9,7 @@ import '../node_modules/react-grid-layout/css/styles.css';
 import '../node_modules/react-resizable/css/styles.css';
 import {XYPlot, VerticalGridLines, HorizontalGridLines, XAxis, YAxis, LineSeries} from 'react-vis';
 import CSVReader from "react-csv-reader";
+import Derivative from './Derivative';
 
 const inputsStructure_manual = [
   {
@@ -103,51 +104,20 @@ const inputsStructure_manual = [
 
 const inputsStructure_preFilled = [{"className":"body","header":"body","inputs":[{"name":"diameter","value":34},{"name":"length","value":46},{"name":"mass","value":59},{"name":"material","value":"cardboard"}]},{"className":"fins","header":"fins","inputs":[{"name":"cant","value":15},{"name":"count","value":7},{"name":"height","value":29},{"name":"mass","value":52},{"name":"material","value":"aluminum"},{"name":"sweep","value":14}]},{"className":"variableMass","header":"variableMass","inputs":[{"name":"distanceFromTip","value":18},{"name":"mass","value":10}]},{"className":"motor","header":"motor","inputs":[{"name":"impulse","value":53},{"name":"mass","value":72},{"name":"burnTime","value":86}]},{"className":"noseCone","header":"noseCone","inputs":[{"name":"length","value":33},{"name":"mass","value":26},{"name":"material","value":"abs"}]}];
 
-/*
-const fromReadMe = {
-  'body': {
-        'diameter': positiveFloat,
-        'length': positiveFloat,
-        'mass': positiveFloat,
-        'material': validMaterial,
-  }, 'fins': {
-        'cant': positiveFloat,
-        'count': positiveFloat,
-        'height': positiveFloat,
-        'mass': positiveFloat,
-        'material': validMaterial,
-        'sweep': positiveFloat,
-  }, 'variableMass': {
-        'distanceFromTip': positiveFloat,
-        'mass': positiveFloat,
-  }, 'motor' : {
-        'impulse': positiveFloat,
-        'mass': positiveFloat,
-  }, 'noseCone': {
-        'length': positiveFloat,
-        'mass': positiveFloat,
-        'material': validMaterial,
-  },
-};
-*/
 const Results = props =>  {
-    const data = [
-      {x: 0, y: 8},
-      {x: 1, y: 5},
-      {x: 2, y: 4},
-      {x: 3, y: 9},
-      {x: 4, y: 1},
-      {x: 5, y: 7},
-      {x: 6, y: 6},
-      {x: 7, y: 3},
-      {x: 8, y: 2},
-      {x: 9, y: 0}
-    ];
-    const layout = [
-      {i: 'a', x: 0, y: 0, w: 2, h: 1, static: true},
-      {i: 'b', x: 1, y: 0, w: 1, h: 1, minW: 2, maxW: 4},
-      {i: 'c', x: 4, y: 0, w: 1, h: 2}
-    ];
+  const layout = [
+    {i: 'a', x: 0, y: 0, w: 2, h: 1, static: true},
+    {i: 'b', x: 1, y: 0, w: 1, h: 1, minW: 2, maxW: 4},
+    {i: 'c', x: 4, y: 0, w: 1, h: 2}
+  ];
+
+  const d1 = props.data;
+  const d2 = props.data2;
+  const d1_ = Derivative(d1);
+  const d2_ = Derivative(d2);
+  const d1__ = Derivative(d1_);
+  const d2__ = Derivative(d2_);
+
   return (
     <GridLayout className="layout" layout={layout} cols={3} rowHeight={300} width={window.innerWidth} height={window.innerHeight}>
       <div key="a" className="Results">
@@ -156,15 +126,29 @@ const Results = props =>  {
           <HorizontalGridLines />
           <XAxis title="Time (s)"/>
           <YAxis title="Altitude"/>
-          <LineSeries data={props.data} />
-          <LineSeries data={props.data2} />
+          <LineSeries data={d1} />
+          <LineSeries data={d2} />
         </XYPlot>
       </div>
       <div key="b" className="Results">
-        Test
+        <XYPlot className="plot" height={(layout[0]["h"] * 300) - 40} width={(window.innerWidth/3) * layout[0]["w"] - 30}>
+          <VerticalGridLines />
+          <HorizontalGridLines />
+          <XAxis title="Time (s)"/>
+          <YAxis title="Velocity"/>
+          <LineSeries data={d1_} />
+          <LineSeries data={d2_} />
+        </XYPlot>
       </div>
       <div key="c" className="Results">
-        Test
+        <XYPlot className="plot" height={(layout[0]["h"] * 300) - 40} width={(window.innerWidth/3) * layout[0]["w"] - 30}>
+          <VerticalGridLines />
+          <HorizontalGridLines />
+          <XAxis title="Time (s)"/>
+          <YAxis title="Acceleration"/>
+          <LineSeries data={d1__} />
+          <LineSeries data={d2__} />
+        </XYPlot>
       </div>
     </GridLayout>
   )
