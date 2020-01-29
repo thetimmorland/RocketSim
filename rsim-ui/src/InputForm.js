553682f864bedb44a51e16507c251f7728742333
Timothy Morland <thetimmorland@gmail.com>
2020-01-28 19:19:12 -0500

    setup form submit

--- rsim-ui/src/InputForm.js
+++ rsim-ui/src/InputForm.js
@@ -12,27 +12,31 @@ import NavigationIcon from "@material-ui/icons/Navigation";
 const useStyles = makeStyles(theme => ({
   root: {
     margin: theme.spacing(2),
-    display: "flex",
-    flexWrap: "wrap"
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-evenly',
   },
   paper: {
     margin: theme.spacing(2),
-    display: "flex",
-    flexDirection: "column"
+    flexGrow: 1,
+    display: 'flex',
+    flexDirection: 'column',
   },
   heading: {
-    margin: theme.spacing(2)
+    marginTop: theme.spacing(2),
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
   },
   input: {
-    margin: theme.spacing(2)
+    margin: theme.spacing(2),
   },
   fab: {
-    position: "absolute",
+    position: 'fixed',
     bottom: theme.spacing(2),
-    right: theme.spacing(2)
+    right: theme.spacing(2),
   },
   fabIcon: {
-    marginRight: theme.spacing(1)
+    marginRight: theme.spacing(1),
   }
 }));
 
@@ -61,7 +65,16 @@ export default function InputForm() {
     setState({ ...state, [event.target.name]: event.target.value });
 
   const handleSubmit = event => {
-    console.log(state);
+    fetch(process.env.API_HOST || 'http://localhost:5000', {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(state),
+    })
+      .then(res => res.json())
+      .then(json => {
+        console.log(json)
+      })
+
     event.preventDefault();
   };
 
@@ -69,135 +82,135 @@ export default function InputForm() {
     <form onSubmit={handleSubmit}>
       <div className={classes.root}>
         <Paper className={classes.paper}>
-          <Typography variant="h6" className={classes.heading}>
+          <Typography variant='h6' className={classes.heading}>
             Body
           </Typography>
           <TextField
-            name="bodyDiameter"
-            label="Diameter (m)"
+            name='bodyDiameter'
+            label='Diameter (m)'
             value={state.bodyDiameter}
             onChange={handleChange}
             className={classes.input}
-            type="number"
+            type='number'
           />
           <TextField
-            name="bodyLength"
-            label="Length (m)"
+            name='bodyLength'
+            label='Length (m)'
             value={state.bodyLength}
             onChange={handleChange}
             className={classes.input}
-            type="number"
+            type='number'
           />
           <TextField
-            name="bodyMass"
-            label="Mass (kg)"
+            name='bodyMass'
+            label='Mass (kg)'
             value={state.bodyMass}
             onChange={handleChange}
             className={classes.input}
-            type="number"
+            type='number'
           />
         </Paper>
         <Paper className={classes.paper}>
-          <Typography variant="h6" className={classes.heading}>
+          <Typography variant='h6' className={classes.heading}>
             Fins
           </Typography>
           <TextField
-            name="finCount"
-            label="Count (m)"
+            name='finCount'
+            label='Count'
             value={state.finCount}
             onChange={handleChange}
             className={classes.input}
-            type="number"
+            type='number'
           />
           <TextField
-            name="finHeight"
-            label="Height"
+            name='finHeight'
+            label='Height (m)'
             value={state.finHeight}
             onChange={handleChange}
             className={classes.input}
-            type="number"
+            type='number'
           />
           <TextField
-            name="finMass"
-            label="Mass"
+            name='finMass'
+            label='Mass (kg)'
             value={state.finMass}
             onChange={handleChange}
             className={classes.input}
-            type="number"
+            type='number'
           />
           <TextField
-            name="finSweep"
-            label="Sweep Angle"
+            name='finSweep'
+            label='Sweep Angle (°)'
             value={state.finSweep}
             onChange={handleChange}
             className={classes.input}
-            type="number"
+            type='number'
           />
         </Paper>
         <Paper className={classes.paper}>
-          <Typography variant="h6" className={classes.heading}>
+          <Typography variant='h6' className={classes.heading}>
             Motor
           </Typography>
           <TextField
-            name="motorBurnTime"
-            label="Burn Time (s)"
+            name='motorBurnTime'
+            label='Burn Time (s)'
             value={state.motorBurnTime}
             onChange={handleChange}
             className={classes.input}
-            type="number"
+            type='number'
           />
           <TextField
-            name="motorImpulse"
-            label="Impulse (N*m)"
+            name='motorImpulse'
+            label='Impulse (N⋅m)'
             value={state.motorImpulse}
             onChange={handleChange}
             className={classes.input}
-            type="number"
+            type='number'
           />
           <TextField
-            name="motorMass"
-            label="Mass (kg)"
+            name='motorMass'
+            label='Mass (kg)'
             value={state.motorMass}
             onChange={handleChange}
             className={classes.input}
-            type="number"
+            type='number'
           />
         </Paper>
         <Paper className={classes.paper}>
-          <Typography variant="h6" className={classes.heading}>
+          <Typography variant='h6' className={classes.heading}>
             Nose
           </Typography>
           <TextField
-            name="noseDiameter"
-            label="Diameter (m)"
+            name='noseDiameter'
+            label='Diameter (m)'
             value={state.noseDiameter}
             onChange={handleChange}
             className={classes.input}
-            type="number"
+            type='number'
           />
           <TextField
-            name="noseLength"
-            label="Length (m)"
+            name='noseLength'
+            label='Length (m)'
             value={state.noseLength}
             onChange={handleChange}
             className={classes.input}
-            type="number"
+            type='number'
           />
           <TextField
-            name="noseMass"
-            label="Mass"
+            name='noseMass'
+            label='Mass (kg)'
             value={state.noseMass}
             onChange={handleChange}
             className={classes.input}
-            type="number"
+            type='number'
           />
         </Paper>
       </div>
       <Fab
         className={classes.fab}
-        type="submit"
-        variant="extended"
-        color="primary"
+        type='submit'
+        variant='extended'
+        color='primary'
       >
         <NavigationIcon className={classes.fabIcon} />
         Simulate
